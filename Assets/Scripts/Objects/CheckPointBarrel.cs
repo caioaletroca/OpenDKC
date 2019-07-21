fcd3a53e21ecd60e9c77384e2f244a4265f9fec9
Caio Alexandre Troti Caetano <caio_aletroca@hotmail.com>
2019-07-21 13:00:47 -0300

    Created animation for Checkpoint barrel

---
+++ Assets/Scripts/Objects/CheckPointBarrel.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public class CheckPointBarrel : MonoBehaviour
+{
+    #region State Variables
+
+    /// <summary>
+    /// A flag that represents if the barrel is spawning the Player
+    /// </summary>
+    [HideInInspector]
+    public bool Spawn
+    {
+        get
+        {
+            return animator.GetBool("Spawn");
+        }
+        set
+        {
+            animator.SetBool("Spawn", value);
+        }
+    }
+
+    #endregion
+
+    #region Public Properties
+
+    public GameObject DustEffect;
+
+    public GameObject CrackedEffect;
+
+    #endregion
+
+    #region Private Methods
+
+    /// <summary>
+    /// Instance for the game object animator
+    /// </summary>
+    private Animator animator;
+
+    #endregion
+
+    #region Unity Events
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    public void OnSpawnFinished()
+    {
+        // Destroy it self
+        Destroy(this);
+
+        // TODO: Spawn effects
+    }
+
+    #endregion
+}
