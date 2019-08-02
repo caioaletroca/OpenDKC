266526efc42ca29bd06dc6ef205fc5def697dbd1
Caio Alexandre Troti Caetano <caio_aletroca@hotmail.com>
2019-08-01 23:49:22 -0300

    Added sound effects to Klampon

--- Assets/Scripts/Characters/Enemies/Klampon.cs
+++ Assets/Scripts/Characters/Enemies/Klampon.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using UnityEngine;
 
 /// <summary>
 /// Controls the bitten enemy
@@ -14,7 +10,56 @@ public class Klampon : Neek
     /// <summary>
     /// Represents if klampon has bitten the player
     /// </summary>
-    public void BiteTrigger() => animator.SetTrigger("BiteTrigger");
+    public bool Bite
+    {
+        get => animator.GetBool("Bite");
+        set => animator.SetBool("Bite", value);
+    }
+
+    #endregion
+
+    #region Private Properties
+
+    /// <summary>
+    /// Instance for the audio source
+    /// </summary>
+    protected AudioSource audioSource;
+
+    #endregion
+
+    #region Unity Methods
+
+    private new void Awake()
+    {
+        base.Awake();
+
+        audioSource = GetComponent<AudioSource>();
+
+        // Start state machine
+        SceneSMB<Klampon>.Initialise(animator, this);
+
+        Damager.Enable();
+    }
+
+    private void FixedUpdate()
+    {
+        // Do not move if in bite state
+        if (Bite)
+            return;
+
+        var direction = transform.localScale.x * -1;
+
+        mRigidBody2D.velocity = new Vector2(direction * Speed, mRigidBody2D.velocity.y);
+    }
+
+    #endregion
+
+    #region Perform Methods
+
+    /// <summary>
+    /// Performs the bite sound effect
+    /// </summary>
+    public void PerformBiteSound() => audioSource.Play();
 
     #endregion
 }
---
+++ Assets/Scripts/Characters/Enemies/StateMachineBehaviours/KlamponWalkingSMB.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Controls the walking state for the Klampon
+/// </summary>
+public class KlamponWalkingSMB : SceneSMB<Klampon>
+{
+    public override void OnSLStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        mMonoBehaviour.Bite = false;
+    }
+}
