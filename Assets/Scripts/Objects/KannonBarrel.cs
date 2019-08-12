11f6c1277e5f690e0d7467917f2fbbffb33b2d53
Caio Alexandre Troti Caetano <caio_aletroca@hotmail.com>
2019-08-12 13:22:43 -0300

    Added new Kannon Ball collisions

--- Assets/Scripts/Objects/KannonBarrel.cs
+++ Assets/Scripts/Objects/KannonBarrel.cs
@@ -1,12 +1,45 @@
-﻿/// <summary>
+﻿using UnityEngine;
+/// <summary>
 /// Handles the barrel projectile from kannon
 /// </summary>
 public class KannonBarrel : KannonBall
 {
+    #region Public Properties
+
+    /// <summary>
+    /// The layers the projectile will collide and will be destroyed
+    /// </summary>
+    [Tooltip("The layers the projectile will collide and will be destroyed.")]
+    public LayerMask CollisionLayer;
+
+    #endregion
+
+    #region Unity Methods
+
+    protected void OnTriggerEnter2D(UnityEngine.Collider2D collision)
+    {
+        if(CollisionLayer.Contains(collision.gameObject))
+        {
+            Explode();
+        }
+    }
+
+    #endregion
+
     #region Event Methods
 
     public void OnDamageableEvent(Damager damager, Damageable damageable)
     {
+        Explode();
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    public void Explode()
+    {
+        // Return object to the pool
         bulletPoolObject.ReturnToPool();
     }
 
